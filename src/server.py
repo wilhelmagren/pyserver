a29762fb8ce0d782529622da4d4c736958a226dc
wagren <wagren@kth.se>
2021-09-24 23:10:06 +0200

    stuff

--- src/server.py
+++ src/server.py
@@ -1,7 +1,22 @@
 import os
-import requests
+import time
+import socket
+
+
+class HTTPServer:
+    def __init__(self, HOST, PORT, **kwargs):
+        self.hostname, self.port = HOST, PORT
+
+    def run(self):
+        print("[*] {}  starting HTTPServer on {}".format(time.asctime(), self.HOST))
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.HOST, self.PORT))
+        server_socket.listen(1)
+        print("[*] {}  listening in port {} ...".format(time.asctime(), self.PORT))
 
 
 if __name__ == "__main__":
     #& Run server
-    print("[*] starting gymbook server on 127.0.0.1:80")
+    server = HTTPServer()
+    server.run()
